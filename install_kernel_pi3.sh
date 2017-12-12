041d4e3f0d70aa90fe9e48bb4c813552746b8fe5
xuoman <12872704+xuoman@users.noreply.github.com>
2017-12-12 00:44:45 +0000

    Add scripts

---
+++ install_kernel_pi3.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+KERNEL=kernel7
+INST_PATH="/"
+CCOPT=""
+INSTOPT=""
+for OPT in "$@"; do
+	case "$OPT" in
+		"-a" )
+			if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+                echo "$PROGNAME: option requires an argument -- $1" 1>&2
+                exit 1
+            fi
+			case "$2" in
+				"pi" | "pi2" ) KERNEL=kernel ;;
+				* ) KERNEL=kernel7 ;;
+			esac
+            shift 2
+			;;
+		"-i" )
+			if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+                echo "$PROGNAME: option requires an argument -- $1" 1>&2
+                exit 1
+            fi
+			INST_PATH="${2}"
+			INSTOPT="INSTALL_MOD_PATH=${INST_PATH}"
+            shift 2
+			;;
+		"-cc" )
+			CCOPT="ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-"
+			;;
+		"-h" )
+			echo "	-a [arch]		select architecture from pi/pi2/pi3"
+			echo "				(default pi3)"
+			echo "	-i [path]		install pash"
+			echo "				(default local install)"
+			echo "	-cc			cross compile option"
+			echo "	-h			show this help"
+			exit 0
+			;;
+	esac
+done
+if [ -n "${CCOPT}" ] && [ "${INST_PATH}" = "/" ] ; then
+	echo "you must set install path"
+	exit 1
+fi
+
+if ! [ -d ${INST_PATH}/boot ] || ! [ -d ${INST_PATH}/boot/overlays ]; then
+	mkdir -p ${INST_PATH}/boot/overlays
+fi
+if ! [ -d ${INST_PATH} ] ; then
+	echo "no such directry"
+	exit 1
+fi
+
+sudo make ${CCOPT} ${INSTOPT} modules_install
+if [ -f ${INST_PATH}/boot/$KERNEL.img ] ; then
+	sudo cp ${INST_PATH}/boot/$KERNEL.img ${INST_PATH}/boot/$KERNEL-backup.img
+fi
+sudo scripts/mkknlimg arch/arm/boot/zImage ${INST_PATH}/boot/$KERNEL.img
+sudo cp arch/arm/boot/dts/*.dtb ${INST_PATH}/boot/
+sudo cp arch/arm/boot/dts/overlays/*.dtb* ${INST_PATH}/boot/overlays/
+sudo cp arch/arm/boot/dts/overlays/README ${INST_PATH}/boot/overlays/
---
+++ make_kernel_pi3.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+KERNEL=kernel7
+MAKECLEAN=0
+MENUCONF=0
+CCOPT=""
+for OPT in "$@"; do
+	case "$OPT" in
+		"-a" )
+			if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+                echo "$PROGNAME: option requires an argument -- $1" 1>&2
+                exit 1
+            fi
+			case "$2" in
+				"pi" | "pi2" )
+					KERNEL=kernel ;;
+				"pi3" )
+					KERNEL=kernel7 ;;
+				* )
+					echo "select from pi/pi2/pi3"
+					exit 1
+					;;
+			esac
+            shift 2
+			;;
+		"-m" )
+			MENUCONF=1
+			;;
+		"-c" )
+			MAKECLEAN=1
+			;;
+		"-cc" )
+			CCOPT="ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-"
+			;;
+		"-h" )
+			echo "	-a [arch]		select architecture from pi/pi2/pi3"
+			echo "				(default pi3)"
+			echo "	-m			do menuconfig"
+			echo "	-c			do make clean"
+			echo "	-cc			cross compile"
+			echo "	-h			show this help"
+			exit 0
+			;;
+	esac
+done
+
+case "$KERNEL" in
+        "kernel" ) make ${CCOPT} bcmrpi_defconfig ;;
+        "kernel7" ) make ${CCOPT} bcm2709_defconfig ;;
+		* ) exit 1 ;;
+esac
+if [ "$MENUCONF" != "0" ] ; then
+	make menuconfig
+fi
+if [ "$MAKECLEAN" != "0" ] ; then
+	make clean
+fi
+if [ -f .config ] ; then
+	make ${CCOPT} -j4 zImage modules dtbs
+else
+	echo "config error"
+fi
